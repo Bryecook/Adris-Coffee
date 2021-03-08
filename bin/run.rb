5c143ed11b2f7b602d6dfc3244e4a74ecc1713ca
Bryecook <brybcook@gmail.com>
2021-03-08 15:00:53 -0800

    Initial add money methods

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,18 @@
 require_relative '../config/environment'
 require 'pry'
 
+def check_balance
+    puts "Your balance is #{client.balance}"
+end
+
+def add_money
+
+end
+
+def make_purchase
+
+end
+
 binding.pry
 
 
