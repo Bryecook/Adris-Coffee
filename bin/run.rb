ab5f19d164569e7ee912b019320c5aca67aede67
Bryecook <brybcook@gmail.com>
2021-03-09 14:54:56 -0800

    Added loops and password recovery to login

--- bin/run.rb
+++ bin/run.rb
@@ -28,31 +28,64 @@ end
 
 def login_create
 clients = Client.all.map {|client| client.username}
+logged_in = false
     prompt = TTY::Prompt.new
-    login_create = prompt.select("Let's drink coffee!", ["Login", "Sign Up", "Exit"])
+   while logged_in==false do
+     login_create = prompt.select("Let's drink coffee!", ["Login", "Sign Up", "Exit"])
     if (login_create == "Login")
         current_username = prompt.ask("What is your username?")
-        current_password = prompt.mask("What is your password?")
-        if clients.include?(current_username) && Client.all.find_by(password: current_password)
-            puts ("Hello #{current_username}, what would you like to drink?")
-        elsif
-          puts "Incorrect username or password. Please try again or Sign up"
-           
-        end
-
+        if clients.include?(current_username)
+            current_password = prompt.mask("What is your password?") 
+            if Client.all.select {|client| client.username==current_username}.first.password==current_password
+                logged_in=true
+                puts ("Hello #{current_username}, what would you like to drink?")
+            else 
+                forgot_password = prompt.select("Incorrect password. Did you forget your password?", ["Yes", "No"])
+                    if forgot_password == "Yes"
+                        loop do
+                            confirm_username= prompt.ask("Please confirm your username.")
+                            if clients.include?(confirm_username)
+                                restored_password = prompt.mask("Please enter new password.")
+                                Client.all.select {|client| client.username==confirm_username}.first.update(password: restored_password)
+                            break
+                            else
+                                incorrect_username = prompt.select("No username match. Do you want to try again or sign up a new account?", ["Try again", "Sign Up"])
+                                    if incorrect_username=="Try again"
+                                    else
+                                        logged_in=false
+                                        break
+                                    end
+                            end
+                        end
+                    else
+                        logged_in=false
+                        puts "Please try again or sign up for a new account!"
+                    end
+                
+                end
+            else
+                logged_in=false
+                puts "Incorrect username. Please try again."
+            end   
     elsif (login_create == "Sign Up")
+        unused_username = false
+        while unused_username == false do
         new_username = prompt.ask("Username")
         if clients.include?(new_username)
             puts "This username isn't available. Please try another"
-      
+            unused_username = false
+        else
+            unused_username = true
         end 
+        end
         new_password = prompt.mask("Password")
         Client.create(username: new_username, password: new_password)
             puts "Welcome #{new_username}, what would you like to drink?"
-
+            logged_in=true
     else 
         exit!
-    end 
+    end
+    end
 end  
 
 
