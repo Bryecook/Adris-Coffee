6b4aa8f41dfdcca4269639d962d139f7e88c7372
Bryecook <brybcook@gmail.com>
2021-03-09 09:59:01 -0800

    Added location_selection

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,24 @@
 require_relative '../config/environment'
 require 'pry'
 
-binding.pry
+def location_selection
+    cities=City.all.map {|city| city.name}
+    prompt = TTY::Prompt.new
+    city_choice = prompt.select("Please select a city:", cities)
+    locations=Location.all.select {|location| location.city.name==city_choice}
+    location_names=locations.map{|location| location.name}
+    if city_choice=="Austin"
+        location_choice = prompt.select("Please select a location:", location_names)
+        #puts "You have selected #{location_choice} in #{city_choice}! Now it's time to pick your drinks!"
+    else
+        location_choice = prompt.select("Please select a location:", location_names)
+        #puts "You have selected #{location_choice} in #{city_choice}! Now it's time to pick your drinks!"
+    end
+    puts "You have selected #{location_choice} in #{city_choice}! Now it's time to pick your drinks!"
+end
 
+
+#binding.pry
+location_selection
 
 puts "HELLO WORLD"
